f3ef62312e078fa4bbb1e099d6f2c1882327c43f
StephenConnolly <steveconnolly6@gmail.com>
2023-12-02 12:37:29 -0700

    added a switch operator for when + and - are selected

--- index.js
+++ index.js
@@ -41,7 +41,13 @@ function subtract() {
 }
 
 function result() {
-
+    if (num1 !== "" && num2 !== "")
+    switch(operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+}     
 }
 
 
