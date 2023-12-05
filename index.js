2a769e5214395bfdaa0de14a5d04152120d0da09
StephenConnolly <steveconnolly6@gmail.com>
2023-12-04 20:42:56 -0700

    removed a couple of functions and previous variables at the start. Added expression, appendNumber function, calculate and clearInput

--- index.js
+++ index.js
@@ -1,55 +1,25 @@
-let num1 = ""
-let num2 = ""
-let operator = ""
-
-document.getElementById("num1").textContent = num1
-document.getElementById("num2").textContent = num2
-
-function display() {
-    document.getElementById("num1").textContent = num1
-    document.getElementById("num2").textContent = num2
-    document.getElementById("operator").textContent = operator
-    document.getElementById("result").textContent = calculate()
-}
-
-function one() {
-    appendNumber(1)
-}
-
-function two() {
-    appendNumber(2)
-}
+let expression = ""
 
 function appendNumber(number) {
-    if (operator === "") {
-        num1 += number;
-    } else {
-        num2 += number;
+    if (operator == null) alert("Number is missing"); 
+    else if (number != "=") {
+        expression = expression + number;
+        document.getElementById("input").textContent = expression;
+    } else if (number =="=") {
+       document.getElementById("result").textContent = eval(expression);
+       clearInput()  
     }
-    display();
-}
-
-function add() {
-    operator = "+"
-    display()
-}
-
-function subtract() {
-    operator = "-"
-    display()
 }
 
 function calculate() {
-    if (num1 !== "" && num2 !== "")
-    switch(operator) {
-        case "+":
-            return parseFloat(num1) + parseFloat(num2);
-        case "-":
-            return parseFloat(num1) - parseFloat(num2);
-        default:
-            break;
-}     
+    document.getElementById("output").textContent = eval(expression);
+    clearInput();
 }
+
+function clearInput() {
+    document.getElementById("input").textContent = "";
+}     
+
 
 
 
