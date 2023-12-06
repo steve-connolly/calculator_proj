2b492e3676db107cca242c83be9885bb6d6b19e6
StephenConnolly <steveconnolly6@gmail.com>
2023-12-05 22:16:51 -0700

    replaced input as num1 in the else if under appendNumber function. Edited format.

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 let expression = ""
 
 function appendNumber(number) {
-    if (operator == null) alert("Number is missing"); 
+    console.log(number)
+    if (operator == null && number!== "=") 
+        {alert("Number is missing"); } 
     else if (number != "=") {
         expression = expression + number;
-        document.getElementById("input").textContent = expression;
-    } else if (number =="=") {
+        document.getElementById("num1").textContent = expression; 
+    } else { 
+        if (number =="=") {
        document.getElementById("result").textContent = eval(expression);
        clearInput()  
+        }
     }
 }
 
 function calculate() {
-    document.getElementById("output").textContent = eval(expression);
+    document.getElementById("result").textContent = eval(expression);
     clearInput();
 }
 
 function clearInput() {
-    document.getElementById("input").textContent = "";
+    expression = "";
+    operator = null;
+    document.getElementById("num1").textContent = "";
 }     
+
+
 
 
 
